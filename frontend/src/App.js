3968d05e1985ad64a622985b07f3d5c827831c96
Lívia Ribeiro <liviaribeiro14@gmail.com>
2020-08-10 06:49:51 -0300

    Axios added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
 import "./App.css";
 import backgroundImage from "./assets/background.jpg";
 
 import Header from "./components/Header";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desenvolvimento de app",
-    "Front-end web",
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -20,7 +25,7 @@ function App() {
       <img width={300} src={backgroundImage} alt="plano de fundo" />
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
